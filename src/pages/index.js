6032e9706bfbada73a88e1d175133dfd7e60588d
erichodges <14981329+erichodges@users.noreply.github.com>
2019-10-09 16:15:17 -0700

    Testing: Options on CarImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import { Link } from "gatsby";
 import React from "react";
+import CarImage from "../components/CarImage";
 // import Image from "../components/image";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
-    <test />
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <CarImage />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
