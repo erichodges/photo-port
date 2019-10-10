0a378eacf6f098341fef1c62425fb2faf0cc6f79
erichodges <14981329+erichodges@users.noreply.github.com>
2019-10-10 13:24:35 -0700

    Upgrade: gatsby-transformer-cloudinary 0.3.0

--- src/components/CarImage.js
+++ src/components/CarImage.js
@@ -2,21 +2,20 @@ import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
 import React from 'react';
 
- 
 const CarImage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      image: file(name: {eq: "car" }) {
-        cloudinary: childCloudinaryAsset {
-          fluid {
-            ...CloudinaryAssetFluid
-          }
-        }
-      }
-    }
-  `);
+	const data = useStaticQuery(graphql`
+		query {
+			image: file(name: { eq: "car" }) {
+				cloudinary: childCloudinaryAsset {
+					fluid {
+						...CloudinaryAssetFluid
+					}
+				}
+			}
+		}
+	`);
 
-  return <Image fluid={data.image.cloudinary.fluid} alt="car image" />;
+	return <Image fluid={data.image.cloudinary.fluid} alt="car image" />;
 };
 
-export default CarImage
+export default CarImage;
