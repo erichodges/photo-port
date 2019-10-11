83f378d2731373b6233d29456401edf7eeaa3140
erichodges <14981329+erichodges@users.noreply.github.com>
2019-10-11 13:26:33 -0700

    Clean: Renoved Image.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import { Link } from "gatsby";
 import React from "react";
 import CarImage from "../components/CarImage";
-// import Image from "../components/image";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
@@ -12,11 +11,8 @@ const IndexPage = () => (
     <CarImage />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `1920px`, marginBottom: `1.45rem` }}>
-
-    </div>
-    <br />
     <Link to="/page-2/">Go to page 2</Link>
+    <br />
   </Layout>
 )
 
