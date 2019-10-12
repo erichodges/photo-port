10cbf1abc9f293d0dec5881014bec4c2d5769d4e
erichodges <14981329+erichodges@users.noreply.github.com>
2019-10-12 15:00:36 -0700

    Moved: CarImage query to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
-import { Link } from "gatsby";
+import { graphql, Link, useStaticQuery } from "gatsby";
+import Image from 'gatsby-image';
 import React from "react";
-import CarImage from "../components/CarImage";
+// import CarImage from "../components/CarImage";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+
+
+const CarImage = () => {
+	const data = useStaticQuery(graphql`
+		query {
+			image: file(name: { eq: "car" }) {
+				cloudinary: childCloudinaryAsset {
+					fluid(maxWidth: 1920) {
+						...CloudinaryAssetFluid
+					}
+				}
+			}
+		}
+	`);
+
+	return <Image fluid={data.image.cloudinary.fluid} alt="car image" />;
+};
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <div>
+      <CarImage />
+    </div>
     <h1>Hi people</h1>
-    <CarImage />
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
