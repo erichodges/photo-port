2c6517f614fc34ef35fd3f2a1e8e37d6e931e9d1
erichodges <14981329+erichodges@users.noreply.github.com>
2019-10-14 14:58:55 -0700

    Testing: Set up background image on page 2 with a text headline over it

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,37 @@
-import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby";
+import React from "react";
+import BgImage from "../components/BgImage";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage = ({ data }) => (
   <Layout>
+    <BgImage 
+      title="car"
+      fluid={data.image.cloudinary.fluid}
+      overlayColor=""
+    >
+      <h1 style={{color: "white" }}>Headline goes here</h1>
+    </BgImage>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+
   </Layout>
 )
 
+
+export const query = graphql`
+		query {
+			image: file(name: { eq: "car" }) {
+				cloudinary: childCloudinaryAsset {
+					fluid(maxWidth: 1920) {
+						...CloudinaryAssetFluid
+					}
+				}
+			}
+		}
+`;
 export default SecondPage
