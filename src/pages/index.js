af754637b0d52e9bea795c6df6accde5c2b8d40b
Eric Hodges <ehodges2004@gmail.com>
2019-11-14 15:00:32 -0800

    Layout: setup gif logo to work over bgImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,15 @@ import logo from "../../static/evija-logo-1560-white.gif"
 import BgImage from "../components/BgImage"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-console.log(logo)
 
+const styles = {
+  width: "400px",
+}
 const Index = ({ data }) => (
   <Layout>
     <BgImage title="car" fluid={data.image.cloudinary.fluid} overlayColor="">
-      <img src={logo} alt="" />
+      <img style={styles} src={logo} alt="" />
+
       <h1 style={{ color: "white" }}>Headline goes here</h1>
     </BgImage>
     <div>
