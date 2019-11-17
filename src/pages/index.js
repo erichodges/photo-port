ae7f51b3e08be4fd299dfc960a629b1a252fe354
Eric Hodges <ehodges2004@gmail.com>
2019-11-17 15:01:05 -0800

    Layout: Deleted items from source repo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,32 @@
-import { graphql, Link } from "gatsby"
+import { Link } from "gatsby"
 import React from "react"
-import logo from "../../static/evija-logo-1560-white.gif"
-import BgImage from "../components/BgImage"
+// import BgImage from "../components/BgImage"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const styles = {
-  width: "400px",
-}
 const Index = ({ data }) => (
   <Layout>
-    <BgImage title="car" fluid={data.image.cloudinary.fluid} overlayColor="">
-      <img style={styles} src={logo} alt="" />
-
+    {/* <BgImage title="car" fluid={data.image.cloudinary.fluid} overlayColor="">
       <h1 style={{ color: "white" }}>Headline goes here</h1>
-    </BgImage>
+    </BgImage> */}
     <div>
       <SEO title="Page two" />
       <h1>Home page</h1>
       <p>Welcome</p>
       <Link to="/page-2">Page 2</Link>
-      <br />
-      <Link to="/page-3">Page 3</Link>
     </div>
   </Layout>
 )
 
-export const query = graphql`
-  query {
-    image: file(name: { eq: "evija-hero" }) {
-      cloudinary: childCloudinaryAsset {
-        fluid(maxWidth: 1920) {
-          ...CloudinaryAssetFluid
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query {
+//     image: file(name: { eq: "evija-hero" }) {
+//       cloudinary: childCloudinaryAsset {
+//         fluid(maxWidth: 1920) {
+//           ...CloudinaryAssetFluid
+//         }
+//       }
+//     }
+//   }
+// `
 export default Index
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,32 +1,29 @@
-import { graphql, Link, useStaticQuery } from "gatsby"
-import Image from "gatsby-image"
+import { Link } from "gatsby"
 import React from "react"
 // import CarImage from "../components/CarImage";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const CarImage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      image: file(name: { eq: "car" }) {
-        cloudinary: childCloudinaryAsset {
-          fluid(maxWidth: 1920) {
-            ...CloudinaryAssetFluid
-          }
-        }
-      }
-    }
-  `)
+// const CarImage = () => {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       image: file(name: { eq: "car" }) {
+//         cloudinary: childCloudinaryAsset {
+//           fluid(maxWidth: 1920) {
+//             ...CloudinaryAssetFluid
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  return <Image fluid={data.image.cloudinary.fluid} alt="car image" />
-}
+//   return <Image fluid={data.image.cloudinary.fluid} alt="car image" />
+// }
 
 const PageTwo = () => (
   <Layout>
     <SEO title="Home" />
-    <div>
-      <CarImage />
-    </div>
+    <div></div>
     <h1>Page 2</h1>
 
     <Link to="/">Home page</Link>
